e833dae7cf0ad16e6d5716cf995cd373ff18c0d9
GGTechGuru <ggtechguru@gmail.com>
2015-07-12 10:45:12 -0300

    Added web services

---
+++ Java/Servlets/GgServlet301/src/net/codejava/Servlet301.java
@@ -0,0 +1,31 @@
+package net.codejava;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet301
+ */
+@WebServlet("/Servlet301")
+public class Servlet301 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet301() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("GG Servlet\nID: 301");
+	}
+}
